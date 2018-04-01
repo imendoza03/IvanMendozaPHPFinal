aabf78ebbfbee0079b7e3e88093aa2232edba06d
imendoza0315 <mendoza.ivaneladio@gmail.com>
2018-04-01 18:28:48 +0200

    second correction to exercise 3

--- exercise_3/index.php
+++ exercise_3/index.php
@@ -1,6 +1,8 @@
 <?php 
 //POST request
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
+//     var_dump($_POST);
+//     die;
     $make = $_POST['make'] ?? null;
     $model = $_POST['model'] ?? null;
     $year = $_POST['year'] ?? null;
@@ -18,9 +20,43 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
         
         $result = json_encode(['response' => 'Car sucessfully created !']);
         echo $result;
+        die();
     }
     
     $result = json_encode(['response' => 'Car could not be created !']);
     echo $result;
+    die();  
 }
 ?>
+
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+<form>
+<label for="make">Make: </label>
+<input type="text" name="make"/>
+<button id="submit-button" type="submit">Submit</button>
+</form>
+<script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+<script type="text/javascript">
+	$(function(){
+		$("form").on('submit', function(event){
+			event.preventDefault();
+			$.ajax({
+				type: "POST",
+				url: '/index.php',
+				data: {'make':'bmw'},
+			}).done(function(data, textStatus, jqXHR){
+				console.log(data);
+			}).fail(function(data){
+				console.log(data);
+			});
+		});
+	});
+</script>
+</body>
+</html>
